646c58693fa2d1e80b5345698dc071a52c42aeed
Hani Alherz <hanialherz1@gmail.com>
2026-01-29 11:16:26 +0300

    updating next.js and adding the darkmode lightmode toggle, with the intial adaptation based on browser

--- src/features/dashboard/DeleteBudgetConfirm.tsx
+++ src/features/dashboard/DeleteBudgetConfirm.tsx
@@ -1,30 +1,51 @@
 "use client";
-import { Description, Dialog, DialogBackdrop, DialogPanel, DialogTitle } from '@headlessui/react';
-import { useState } from 'react';
+import {
+  Description,
+  Dialog,
+  DialogBackdrop,
+  DialogPanel,
+  DialogTitle,
+} from "@headlessui/react";
+import { useState } from "react";
 
-interface props {budgetName:string}
+interface props {
+  budgetName: string;
+}
 
-const DeleteBudgetConfirm=({budgetName}:props)=> {
-  const [isOpen, setIsOpen] = useState(false)
+const DeleteBudgetConfirm = ({ budgetName }: props) => {
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <>
-      <button className='cursor-pointer' onClick={() => setIsOpen(true)}>Delete</button>
-      <Dialog open={isOpen} onClose={() => setIsOpen(false)} className="relative z-50">
+      <button className="cursor-pointer" onClick={() => setIsOpen(true)}>
+        Delete
+      </button>
+      <Dialog
+        open={isOpen}
+        onClose={() => setIsOpen(false)}
+        className="relative z-50"
+      >
         <DialogBackdrop className="fixed inset-0 bg-black/60 not-dark:bg-black/20" />
         <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
           <DialogPanel className="max-w-lg space-y-4 bg-l-black text-foreground rounded-2xl p-6 not-dark:bg-[#F2F3F4] not-dark:text-[#808080]">
-            <DialogTitle className="font-bold not-dark:text-[#5c5c5c]">Delete budget ({budgetName})</DialogTitle>
+            <DialogTitle className="font-bold not-dark:text-[#5c5c5c]">
+              Delete budget ({budgetName})
+            </DialogTitle>
             <Description>This will permanently delete the budget</Description>
             <div className="flex items-center gap-4">
               <button onClick={() => setIsOpen(false)}>Cancel</button>
-              <button className="px-4 shadow-2xl shadow-main/30 bg-inverted-main text-foreground text-xl font-medium cursor-pointer py-2 rounded-xl transition-all duration-300 hover:bg-transparent hover:inset-ring-1 hover:inset-ring-inverted-main active:bg-transparent active:inset-ring-1 active:inset-ring-inverted-main not-dark:text-[#fff] not-dark:hover:text-[#5c5c5c] not-dark:active:text-[#5c5c5c] md:col-span-full" onClick={() => setIsOpen(false)}>Delete</button>
+              <button
+                className="px-4 shadow-2xl shadow-main/30 bg-inverted-main text-foreground text-xl font-medium cursor-pointer py-2 rounded-xl transition-all duration-300 hover:bg-transparent hover:inset-ring-1 hover:inset-ring-inverted-main active:bg-transparent active:inset-ring-1 active:inset-ring-inverted-main not-dark:text-white not-dark:hover:text-[#5c5c5c] not-dark:active:text-[#5c5c5c] md:col-span-full"
+                onClick={() => setIsOpen(false)}
+              >
+                Delete
+              </button>
             </div>
           </DialogPanel>
         </div>
       </Dialog>
     </>
-  )
-}
+  );
+};
 
 export default DeleteBudgetConfirm;
